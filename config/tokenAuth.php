70ed5aedb6fedb08223292f05dd999e0823c0a48
Matthias Thalmann <matthiasthalmann1@hotmail.de>
2023-08-12 23:37:19 +0200

    tests(token-auth-manager): added tests for TokenAuthManager

--- config/tokenAuth.php
+++ config/tokenAuth.php
@@ -5,19 +5,19 @@ return [
      * The amount of minutes after which a token expires
      */
     'expiration_minutes' => [
-        'refresh' => env('TOKEN_AUTH_REFRESH_EXPIRATION', 60 * 24 * 7),
-        'access' => env('TOKEN_AUTH_ACCESS_EXPIRATION', 10),
+        'refresh' => 60 * 24 * 7,
+        'access' => 10,
     ],
 
     /*
      * The amount of hours after which a revoked token is pruned
      */
     'prune_revoked_after_hours' => [
-        'refresh' => env('TOKEN_AUTH_PRUNE_REVOKED_AFTER_HOURS', 24 * 7),
+        'refresh' => 24 * 7,
     ],
 
     /**
      * Whether or not to run the migrations
      */
-    'run_migrations' => env('TOKEN_AUTH_RUN_MIGRATIONS', true),
+    'run_migrations' => true,
 ];
---
+++ tests/Unit/TokenAuthManagerTest.php
@@ -0,0 +1,385 @@
+<?php
+
+namespace TokenAuth\Tests\Unit;
+
+use Illuminate\Contracts\Auth\Authenticatable;
+use InvalidArgumentException;
+use Mockery;
+use Mockery\MockInterface;
+use stdClass;
+use TokenAuth\Concerns\HasAuthTokens;
+use TokenAuth\Enums\TokenType;
+use TokenAuth\Contracts\AuthTokenBuilderContract;
+use TokenAuth\Contracts\AuthTokenContract;
+use TokenAuth\Models\AuthToken;
+use TokenAuth\Support\AuthTokenPairBuilder;
+use TokenAuth\Support\NewAuthToken;
+use TokenAuth\Support\TokenGuard;
+use TokenAuth\Tests\TestCase;
+use TokenAuth\TokenAuthManager;
+
+/**
+ * @covers \TokenAuth\TokenAuthManager
+ *
+ * @uses \TokenAuth\TokenAuthServiceProvider
+ * @uses \TokenAuth\Facades\TokenAuth
+ * @uses \TokenAuth\Enums\TokenType
+ * @uses \TokenAuth\Support\AbstractTokenGuard
+ * @uses \TokenAuth\Support\AuthTokenPairBuilder
+ * @uses \TokenAuth\Support\NewAuthToken
+ * @uses \TokenAuth\Support\NewAuthTokenPair
+ */
+class TokenAuthManagerTest extends TestCase {
+    private TokenAuthManager $manager;
+
+    public function setUp(): void {
+        parent::setUp();
+
+        $this->manager = new TokenAuthManager();
+    }
+
+    public function testGetAuthTokenClassReturnsDefaultClassIfNoOtherSet(): void {
+        $this->assertEquals(
+            AuthToken::class,
+            $this->manager->getAuthTokenClass()
+        );
+    }
+
+    public function testUseAuthTokenSetsTheAuthTokenClass(): void {
+        $authTokenClass = new class extends AuthToken {};
+
+        $this->manager->useAuthToken($authTokenClass::class);
+
+        $this->assertEquals(
+            $authTokenClass::class,
+            $this->manager->getAuthTokenClass()
+        );
+    }
+
+    public function testUseAuthTokenFailsIfClassDoesNotImplementAuthTokenContract(): void {
+        $this->expectException(InvalidArgumentException::class);
+
+        $this->manager->useAuthToken(stdClass::class);
+    }
+
+    public function testGetTokenGuardClassReturnsDefaultClassIfNoOtherSet(): void {
+        $this->assertEquals(
+            TokenGuard::class,
+            $this->manager->getTokenGuardClass()
+        );
+    }
+
+    public function testUseTokenGuardSetsTheTokenGuardClass(): void {
+        $this->manager->useTokenGuard(TokenGuardTestClass::class);
+
+        $this->assertEquals(
+            TokenGuardTestClass::class,
+            $this->manager->getTokenGuardClass()
+        );
+    }
+
+    public function testUseTokenGuardFailsIfClassDoesNotExtendAbstractTokenGuard(): void {
+        $this->expectException(InvalidArgumentException::class);
+
+        $this->manager->useTokenGuard(stdClass::class);
+    }
+
+    public function testCreateTokenPairReturnsAnAuthTokenPairBuilder(): void {
+        $user = Mockery::mock(Authenticatable::class);
+
+        $generateGroupId = 1337;
+
+        /**
+         * @var AuthTokenBuilderContract|MockInterface
+         */
+        $builderMock = Mockery::mock(AuthTokenBuilderContract::class);
+
+        // 2 times -> 1 for each token
+        $builderMock
+            ->shouldReceive('setAuthenticatable')
+            ->with($user)
+            ->twice();
+        $builderMock
+            ->shouldReceive('setGroupId')
+            ->with($generateGroupId)
+            ->twice();
+
+        /**
+         * @var AuthTokenContract|MockInterface
+         */
+        $authTokenClass = Mockery::mock(AuthTokenContract::class);
+
+        $authTokenClass
+            ->shouldReceive('create')
+            ->with(TokenType::ACCESS)
+            ->once()
+            ->andReturn($builderMock);
+        $authTokenClass
+            ->shouldReceive('create')
+            ->with(TokenType::REFRESH)
+            ->once()
+            ->andReturn($builderMock);
+
+        $authTokenClass
+            ->shouldReceive('generateGroupId')
+            ->with($user)
+            ->once()
+            ->andReturn($generateGroupId);
+
+        $this->manager->useAuthToken($authTokenClass::class);
+
+        $tokenPairBuilder = $this->manager->createTokenPair($user);
+
+        $this->assertInstanceOf(AuthTokenPairBuilder::class, $tokenPairBuilder);
+    }
+
+    public function testCreateTokenPairDoesNotGenerateGroupIdIfDisabled(): void {
+        $user = Mockery::mock(Authenticatable::class);
+
+        /**
+         * @var AuthTokenBuilderContract|MockInterface
+         */
+        $builderMock = Mockery::mock(AuthTokenBuilderContract::class);
+
+        $builderMock->shouldReceive('setAuthenticatable');
+        $builderMock->shouldNotReceive('setGroupId');
+
+        /**
+         * @var AuthTokenContract|MockInterface
+         */
+        $authTokenClass = Mockery::mock(AuthTokenContract::class);
+
+        $authTokenClass
+            ->shouldReceive('create')
+            ->twice()
+            ->andReturn($builderMock);
+
+        $this->manager->useAuthToken($authTokenClass::class);
+
+        $tokenPairBuilder = $this->manager->createTokenPair(
+            $user,
+            generateGroupId: false
+        );
+
+        $this->assertInstanceOf(
+            AuthTokenBuilderContract::class,
+            $tokenPairBuilder
+        );
+    }
+
+    public function testRotateTokenPairRotatesTheTokenAndReturnsAnAuthTokenPairBuilder(): void {
+        $refreshTokenGroupId = 101;
+        $user = Mockery::mock(Authenticatable::class);
+
+        /**
+         * @var AuthTokenBuilderContract|MockInterface
+         */
+        $authTokenBuilder = Mockery::mock(AuthTokenBuilderContract::class);
+        $authTokenBuilder->shouldIgnoreMissing();
+
+        /**
+         * @var AuthTokenContract|MockInterface
+         */
+        $refreshToken = Mockery::mock(AuthTokenContract::class);
+
+        $this->manager->useAuthToken($refreshToken::class);
+
+        $authTokenBuilder
+            ->shouldReceive('build')
+            ->withAnyArgs()
+            ->twice()
+            ->andReturn(new NewAuthToken($refreshToken, ''));
+
+        $refreshToken
+            ->shouldReceive('create')
+            ->with(TokenType::ACCESS)
+            ->once()
+            ->andReturn($authTokenBuilder);
+        $refreshToken
+            ->shouldReceive('create')
+            ->with(TokenType::REFRESH)
+            ->once()
+            ->andReturn($authTokenBuilder);
+
+        $refreshToken
+            ->shouldReceive('getAuthenticatable')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($user);
+        // once in "fromToken" and once when deleting access tokens
+        $refreshToken
+            ->shouldReceive('getGroupId')
+            ->withNoArgs()
+            ->twice()
+            ->andReturn($refreshTokenGroupId);
+        $refreshToken
+            ->shouldReceive('getName')
+            ->withNoArgs()
+            ->once()
+            ->andReturnNull();
+        $refreshToken
+            ->shouldReceive('getAbilities')
+            ->withNoArgs()
+            ->once()
+            ->andReturn([]);
+
+        $refreshToken
+            ->shouldReceive('deleteTokensFromGroup')
+            ->with($refreshTokenGroupId, TokenType::ACCESS)
+            ->once();
+
+        $refreshToken
+            ->shouldReceive('revoke')
+            ->withNoArgs()
+            ->once();
+        // twice from build, once from revoke
+        $refreshToken
+            ->shouldReceive('store')
+            ->withNoArgs()
+            ->times(3);
+
+        $tokenPairBuilder = $this->manager->rotateTokenPair($refreshToken);
+
+        $this->assertInstanceOf(AuthTokenPairBuilder::class, $tokenPairBuilder);
+
+        $tokenPairBuilder->buildPair();
+    }
+
+    public function testRotateTokenPairDoesNotDeleteAccessTokensWhenDisabled(): void {
+        /**
+         * @var AuthTokenContract|MockInterface
+         */
+        $refreshToken = Mockery::mock(AuthTokenContract::class);
+
+        $refreshToken->shouldIgnoreMissing();
+
+        $this->manager->useAuthToken($refreshToken::class);
+
+        /**
+         * @var AuthTokenBuilderContract|MockInterface
+         */
+        $authTokenBuilder = Mockery::mock(AuthTokenBuilderContract::class);
+        $authTokenBuilder->shouldIgnoreMissing();
+
+        $authTokenBuilder
+            ->shouldReceive('build')
+            ->withAnyArgs()
+            ->twice()
+            ->andReturn(new NewAuthToken($refreshToken, ''));
+
+        $refreshToken
+            ->shouldReceive('create')
+            ->withAnyArgs()
+            ->andReturn($authTokenBuilder);
+
+        $tokenPairBuilder = $this->manager->rotateTokenPair(
+            $refreshToken,
+            deleteAccessTokens: false
+        );
+
+        $this->assertInstanceOf(AuthTokenPairBuilder::class, $tokenPairBuilder);
+
+        $refreshToken->shouldNotReceive('deleteTokensFromGroup');
+
+        $tokenPairBuilder->buildPair();
+    }
+
+    /**
+     * @dataProvider tokenTypeProvider
+     */
+    public function testActingAsWithUserAndSpecificTokenType(
+        TokenType $type
+    ): void {
+        $user = $this->createMockUser($type);
+
+        $this->assertNull(auth()->user());
+
+        $token = $this->manager->actingAs($user, tokenType: $type);
+
+        $this->assertEquals($user, auth()->user());
+        $this->assertEquals($type, $token->getType());
+
+        $this->assertAuthenticatedAs($user);
+    }
+
+    public function testActingAsWithNoAbilities(): void {
+        $user = $this->createMockUser(TokenType::ACCESS);
+        $token = $this->manager->actingAs(
+            $user,
+            abilities: [],
+            tokenType: TokenType::ACCESS
+        );
+
+        $this->assertFalse($token->hasAbility('foo'));
+    }
+
+    public function testActingAsWithAllAbilities(): void {
+        $user = $this->createMockUser(TokenType::ACCESS);
+        $token = $this->manager->actingAs($user, abilities: ['*']);
+
+        $this->assertTrue($token->hasAbility('foo'));
+        $this->assertTrue($token->hasAbility('*'));
+    }
+
+    public function testActingAsWithAbilities(): void {
+        $user = $this->createMockUser(TokenType::ACCESS);
+        $token = $this->manager->actingAs($user, ['foo']);
+
+        $this->assertTrue($token->hasAbility('foo'));
+        $this->assertFalse($token->hasAbility('bar'));
+    }
+
+    public function testActingAsWithNoUser(): void {
+        $user = $this->createMockUser(
+            TokenType::ACCESS,
+            withTokenShouldBeCalled: false
+        );
+
+        auth()
+            ->guard(TokenType::ACCESS->getGuardName())
+            ->setUser($user);
+        auth()->shouldUse(TokenType::ACCESS->getGuardName());
+
+        $this->assertEquals($user, auth()->user());
+
+        $this->manager->actingAs(null);
+
+        $this->assertNull(auth()->user());
+    }
+
+    private function createMockUser(
+        TokenType $tokenType,
+        int $id = 1,
+        bool $withTokenShouldBeCalled = true
+    ): TestUser|MockInterface {
+        /**
+         * @var TestUser|MockInterface
+         */
+        $user = Mockery::mock(TestUser::class);
+
+        $user->shouldReceive('getAuthIdentifier')->andReturn($id);
+        $withTokenExpectation = $user
+            ->shouldReceive('withToken')
+            ->withArgs(
+                fn($arg) => $arg instanceof AuthTokenContract &&
+                    $arg->getType() === $tokenType
+            );
+
+        if ($withTokenShouldBeCalled) {
+            $withTokenExpectation->once();
+        }
+
+        return $user;
+    }
+
+    public function tokenTypeProvider() {
+        return array_map(fn(TokenType $type) => [$type], TokenType::cases());
+    }
+}
+
+class TokenGuardTestClass extends TokenGuard {
+}
+
+abstract class TestUser implements Authenticatable {
+    use HasAuthTokens;
+}
